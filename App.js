406c256c95e1946d7624cc7eb74d55d8027a5759
kid-us <kidushh29@gmail.com>
2024-06-01 11:23:26 +0300

    Working with Images

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image } from "react-native";
 
 export default function App() {
   const handlePress = () => {
@@ -7,18 +7,29 @@ export default function App() {
   };
   return (
     <View style={styles.container}>
-      <View style={styles.innerContainer}>
-        <Text
-          onPress={handlePress}
-          numberOfLines={1}
-          style={{ color: "white", fontSize: 20, fontWeight: "bold" }}
-        >
-          Hello World!
-        </Text>
-        <Text style={{ fontSize: 15, fontWeight: "semibold" }}>
-          This is my first React Native App!
-        </Text>
-      </View>
+      <Text
+        onPress={handlePress}
+        numberOfLines={1}
+        style={{ color: "white", fontSize: 20, fontWeight: "bold" }}
+      >
+        Hello World!
+      </Text>
+      {/* <Text style={{ fontSize: 15, fontWeight: "semibold" }}>
+        This is my first React Native App!
+      </Text> */}
+      <Image
+        // blurRadius={1}
+        fadeDuration={1000}
+        style={styles.logo}
+        resizeMode="cover"
+        // Static Image
+        // source={require("./assets/icon.png")}
+        // Images from url
+        source={{
+          uri: "https://picsum.photos/200/300",
+        }}
+      ></Image>
+      <StatusBar style="auto" />
     </View>
   );
 }
@@ -28,11 +39,12 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#fff",
     backgroundColor: "dodgerblue",
-    // alignItems: "center",
-    // justifyContent: "center",
+    alignItems: "center",
+    justifyContent: "center",
   },
-
-  innerContainer: {
-    padding: 30,
+  logo: {
+    width: 400,
+    height: 500,
+    borderRadius: 10,
   },
 });
