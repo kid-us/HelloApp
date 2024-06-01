74e9c6587e9d76eeb44f401d4508e5bbc0b5128c
kid-us <kidushh29@gmail.com>
2024-06-01 10:55:31 +0300

    Config Development Setup

---
+++ App.js
@@ -0,0 +1,23 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={{ color: "white" }}>Hello World!</Text>
+      <Text>This is my first React Native App!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    marginTop: 50,
+    backgroundColor: "dodgerblue",
+    // alignItems: "center",
+    // justifyContent: "center",
+  },
+});
