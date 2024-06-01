bc34d1d2e6674f696940f60fa3ceadf1aa1542fd
kid-us <kidushh29@gmail.com>
2024-06-01 11:05:34 +0300

    Working with Text

--- App.js
+++ App.js
@@ -2,11 +2,14 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  const handlePress = () => {
+    console.log("Hello React Native");
+  };
   return (
     <View style={styles.container}>
       <View style={styles.innerContainer}>
         <Text
-          onPress={() => console.log("Hello World")}
+          onPress={handlePress}
           numberOfLines={1}
           style={{ color: "white", fontSize: 20, fontWeight: "bold" }}
         >
