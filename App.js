4ac2309cd624a1dc3a4bf823c265f23538f9811a
kid-us <kidushh29@gmail.com>
2024-06-02 21:39:48 +0300

    Working with FlexDirection

--- App.js
+++ App.js
@@ -2,10 +2,16 @@ import { StyleSheet, View, Platform, StatusBar } from "react-native";
 
 export default function App() {
   return (
-    <View style={{ backgroundColor: "#fff", flex: 1 }}>
-      <View style={{ backgroundColor: "green", flex: 2 }} />
-      <View style={{ backgroundColor: "yellow", flex: 1 }} />
-      <View style={{ backgroundColor: "tomato", flex: 2 }} />
+    <View
+      style={{
+        backgroundColor: "#fff",
+        flex: 1,
+        flexDirection: "row",
+      }}
+    >
+      <View style={{ backgroundColor: "green", width: 100, height: 100 }} />
+      <View style={{ backgroundColor: "yellow", width: 100, height: 100 }} />
+      <View style={{ backgroundColor: "tomato", width: 100, height: 100 }} />
     </View>
   );
 }
