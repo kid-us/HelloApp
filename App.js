0e2574e1f62ba02af23f089445a41e49c1596eb8
kid-us <kidushh29@gmail.com>
2024-06-02 21:57:19 +0300

    Working with flex flexGrow, FlexShrink, and flexBasis

--- App.js
+++ App.js
@@ -10,22 +10,22 @@ export default function App() {
         justifyContent: "center",
         alignItems: "center",
         alignContent: "center",
-        flexWrap: "wrap",
+        // flexWrap: "wrap",
       }}
     >
       <View
         style={{
           backgroundColor: "green",
-          width: 100,
+          // width: 100,
+          // flexBasis: 100, // width or
+          width: 400,
+          // flex: 1,
           height: 100,
+          flexShrink: 2,
         }}
       />
       <View style={{ backgroundColor: "yellow", width: 100, height: 100 }} />
       <View style={{ backgroundColor: "tomato", width: 100, height: 100 }} />
-      <View style={{ backgroundColor: "gray", width: 100, height: 100 }} />
-      <View
-        style={{ backgroundColor: "greenyellow", width: 100, height: 100 }}
-      />
     </View>
   );
 }
