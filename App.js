4ceb7ff4ff01f05d8d52391702d001e68c702032
kid-us <kidushh29@gmail.com>
2024-06-02 20:36:29 +0300

    Working with Dimensions

--- App.js
+++ App.js
@@ -1,9 +1,28 @@
-import { Text, View } from "react-native";
+import {
+  Dimensions,
+  StyleSheet,
+  View,
+  Platform,
+  StatusBar,
+} from "react-native";
 
 export default function App() {
+  console.log(Dimensions.get("screen"));
   return (
-    <View>
-      <Text>Hello World</Text>
+    <View style={style.container}>
+      <View
+        style={{ backgroundColor: "dodgerblue", width: 150, height: 70 }}
+      ></View>
     </View>
   );
 }
+
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
