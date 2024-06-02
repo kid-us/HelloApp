b34942fad751034210bc163735d059917c12dc32
kid-us <kidushh29@gmail.com>
2024-06-02 21:47:10 +0300

    Working with justifyContent, alignItems and alignSelf

--- App.js
+++ App.js
@@ -7,9 +7,17 @@ export default function App() {
         backgroundColor: "#fff",
         flex: 1,
         flexDirection: "row",
+        justifyContent: "center",
+        alignItems: "center",
       }}
     >
-      <View style={{ backgroundColor: "green", width: 100, height: 100 }} />
+      <View
+        style={{
+          backgroundColor: "green",
+          width: 100,
+          height: 100,
+        }}
+      />
       <View style={{ backgroundColor: "yellow", width: 100, height: 100 }} />
       <View style={{ backgroundColor: "tomato", width: 100, height: 100 }} />
     </View>
