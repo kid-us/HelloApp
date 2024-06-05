3d6802bb216e1e3cf44370e3ac90f367585d511b
kid-us <kidushh29@gmail.com>
2024-06-05 12:45:02 +0300

    Adding Icons to the ViewImageScreen

--- App.js
+++ App.js
@@ -11,5 +11,5 @@ import Card from "./app/components/Card";
 import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
 
 export default function App() {
-  return <ListingDetailsScreen />;
+  return <ViewImageScreen />;
 }
--- app/screens/ViewImageScreen.js
+++ app/screens/ViewImageScreen.js
@@ -1,12 +1,21 @@
 import React from "react";
 import { Image, StyleSheet, View, Platform, StatusBar } from "react-native";
 import colors from "../config/colors";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 function ViewImageScreen(props) {
   return (
     <View style={styles.container}>
-      <View style={styles.closeIcon}></View>
-      <View style={styles.deleteIcon}></View>
+      <View style={styles.closeIcon}>
+        <MaterialCommunityIcons name="close" color="#000" size={30} />
+      </View>
+      <View style={styles.deleteIcon}>
+        <MaterialCommunityIcons
+          name="trash-can-outline"
+          color="#000"
+          size={30}
+        />
+      </View>
       <Image
         style={styles.image}
         source={require("../assets/chair.jpg")}
@@ -18,22 +27,16 @@ function ViewImageScreen(props) {
 
 const styles = StyleSheet.create({
   closeIcon: {
-    width: 50,
-    height: 50,
-    backgroundColor: colors.primary,
     position: "absolute",
     top: 40,
     left: 30,
   },
   container: {
-    backgroundColor: colors.black,
+    // backgroundColor: colors.black,
     marginTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
     flex: 1,
   },
   deleteIcon: {
-    width: 50,
-    height: 50,
-    backgroundColor: colors.secondary,
     position: "absolute",
     top: 40,
     right: 30,
